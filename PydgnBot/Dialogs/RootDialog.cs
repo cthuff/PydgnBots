2dabb1d65748ab6201d5aa2b7e6820a916a2821c
Arsha Ali <squirt999@gmail.com>
2017-09-23 20:06:45 -0400

    entered comments and checks if username is already taken

--- PydgnBot/Dialogs/RootDialog.cs
+++ PydgnBot/Dialogs/RootDialog.cs
@@ -23,27 +23,53 @@ namespace PydgnBot.Dialogs
 
             switch (taskID)
             {
-                case 0:
+                //first time user prompt
+                case 0: 
                     await context.PostAsync("Welcome to Pydgn! We see this is your first time here. Please choose the username you'd like people to contact you at.");
                     taskID = 1;
                     break;
+                //this is where the user creates their username
                 case 1:
-                    if (activity.Text.Contains("@") || activity.Text.Contains(" ") || activity.Text.Contains("pydgn"))
+                    if (activity.Text.Contains("@") || activity.Text.Contains(" "))
                     {
                         await context.PostAsync($"Your username is Invalid. Please enter one without spaces or the @ symbol");                 
+                    }
+                    else if (activity.Text.Contains("pydgn")) //|| dataBase.contains(activity.Text)) //this checks to see if the username is already taken
+                    {
+                        await context.PostAsync("The username you entered is already taken. Please enter a different username");
                     }
                     else
                     {
                         await context.PostAsync($"Your username is @{activity.Text} \n To send a message, it needs to be formatted as \"@Username message goes here...\" ");
+                        //this is the username stored in activity.Name
+                        activity.Name = activity.Text;
+                        //the username needs to be added to the database
+                        //this line sends the username to the server
+                        
                         taskID = 2;
                     }
                     break;
                 case 2:
-                    await context.PostAsync("To send a message, it needs to be formatted as \"@Username message goes here...\" ");
+                    if (activity.Text.StartsWith("@") == false )
+                    {
+                        await context.PostAsync("To send a message, it needs to be formatted as \"@Username message goes here...\" If you need help, type \"HELP\" ");
+                        context.Wait(MessageReceivedAsync);
+                        if (activity.Text.Starts)
+                    }
+                    else
+                    {
+
+                    }
+
                     break;
                 default:
                     await context.PostAsync("Shoot, it broke");
                     break;
+            }
+
+                Help:
+            {
+
             }
             context.Wait(MessageReceivedAsync);
         }
